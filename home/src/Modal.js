b1b9ade8727e67a446d19df1dc3d25e2210b9c0a
leroy-j <38360895+leroy-j@users.noreply.github.com>
2018-10-11 14:25:55 -0600

    Update Modal.js

--- home/src/Modal.js
+++ home/src/Modal.js
@@ -5,37 +5,37 @@ class Modal extends Component{
     return (
       <div className="DialogHolder">
         <div className="mini-modal" style={{display:'block'}}>
-	    	  <div className="mini-modal-body">
-					  <div className="mini-modal-main">
-						  <div className="mini-modal-header">
-							  <h5 className="mini-modal-title">Preview</h5>
+	  <div className="mini-modal-body">
+            <div className="mini-modal-main">
+              <div className="mini-modal-header">
+		<h5 className="mini-modal-title">Preview</h5>
                 <button onClick={this.props.onHideModal} className="close-preview">
                   <span>×</span>
               	</button>
               </div>
               <div className="mini-modal-content">
-	              <p className="preview-book-title">
+	        <p className="preview-book-title">
                   {this.props.selectedSearch.title }
                 </p>	
-		          <div className="left">
-		            <div className="book-cover" style={{width:'128px',height:'192px',backgroundImage: 'url("'+this.props.selectedSearch.imageLinks.thumbnail+'")'
-			}}></div>
-			<p>
-			 {
-			     ((this.props.selectedSearch.industryIdentifiers).length && (
-			     this.props.selectedSearch.industryIdentifiers.map( (mod,i)=>
-			      <span className="isbn"  key={i}>
-				{(mod.type).toString().toUpperCase()}: {mod.identifier}<br />
-			      </span>   
-			      )))	
-			  }
-			</p>
-		      </div>
-			<div className="right">
-				<p className="publish">
+		<div className="left">
+		  <div className="book-cover" style={{width:'128px',height:'192px',backgroundImage: 'url("'+this.props.selectedSearch.imageLinks.thumbnail+'")'}}>
+                  </div>
+		<p>
+		{
+                 ((this.props.selectedSearch.industryIdentifiers).length && (
+		  this.props.selectedSearch.industryIdentifiers.map( (mod,i)=>
+		    <span className="isbn"  key={i}>
+		      {(mod.type).toString().toUpperCase()}: {mod.identifier}<br />
+		    </span>   
+		  )))	
+		}
+		</p>
+	     </div>
+			  <div className="right">
+				  <p className="publish">
 					  {this.props.selectedSearch.publisher }<br />
 					  {this.props.selectedSearch.publishedDate}
-                    </p>
+          </p>
 				<p>Description:</p>	
 					<span className="preview-book-description"> 
 						{this.props.selectedSearch.description}
