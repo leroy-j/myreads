6036e63ecfc744d8ae1414d9f2248cf60cf45e60
leroy-j <38360895+leroy-j@users.noreply.github.com>
2018-10-11 11:39:37 -0600

    Update Modal.js

--- home/src/Modal.js
+++ home/src/Modal.js
@@ -1,34 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const modalRoot = ReactDOM.createElement('div');
-
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-    // Create a div that we'll render the modal into. Because each
-    // Modal component has its own element, we can render multiple
-    // modal components into the modal container.
-    this.el = document.createElement('div');
-  }
-
-  componentDidMount() {
-    // Append the element into the DOM on mount. We'll render
-    // into the modal container element (see the HTML tab).
-    modalRoot.appendChild(this.el);
-  }
-
-  componentWillUnmount() {
-    // Remove the element from the DOM when we unmount
-    modalRoot.removeChild(this.el);
-  }
-  
-  render() {
-    // Use a portal to render the children into the element
-    return ReactDOM.createPortal(
-      // Any valid React child: JSX, strings, arrays, etc.
-      this.props.children,
-      // A DOM element
-      this.el,
-    );
+class Modal extends Component{
+  render(){ console.log('Modal:',this.props)
+    return (
+        <div className="DialogHolder">
+          <div className="modal" style={{display:'block'}}>
+		    <div className="modal-body">
+			  <div className="modal-main">
+			    <div className="modal-header">
+				  <h5 className="modal-title">Preview</h5>
+                  <button onClick={this.props.onHideModal} className="close">
+                    <span>×</span>
+                	</button>
+                </div>
+                <div className="modal-content">
+				  <p className="preview-book-title">
+                    {this.props.selectedSearch.title }
+                  </p>	
+				  <div className="left">
+				    <div 
+					  className="book-cover" 
+					  href="/preview?id=zjTc4R2AGyIC" 
+					  style={{
+							    width: '128px', 
+								height: '192px', 
+								backgroundImage: 'url("'+this.props.selectedSearch.imageLinks.thumbnail+'")'
+							}}></div>
+					<p>
+					  {
+						((this.props.selectedSearch.industryIdentifiers).length && (
+						  this.props.selectedSearch.industryIdentifiers.map( (mod,i)=>
+					      <span className="isbn"  key={i}>
+							{(mod.type).toString().toUpperCase()}:																		    {mod.identifier}<br />
+						  </span>   
+						  )
+						 ))	
+						}
+					</p>
+				 </div>
+				 <div className="right">
+					<p className="publish">
+					  {this.props.selectedSearch.publisher }<br />
+					  {this.props.selectedSearch.publishedDate}
+                    </p>
+				   <p>Description:</p>	
+					<span className="preview-book-description"> 
+						{this.props.selectedSearch.description}
+					</span>
+				 </div>
+                </div>
+              </div>
+		    </div>
+      	  </div>
+        </div>
+    )
   }
 }
+export default Modal
