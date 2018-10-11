28f6f081c6b1faf2445efeb61ffb95cc52315aa7
leroy-j <38360895+leroy-j@users.noreply.github.com>
2018-10-11 10:22:11 -0600

    Update App.js

--- home/src/App.js
+++ home/src/App.js
@@ -7,19 +7,18 @@ import './App.css'
 import './Rating.css'
 import ListBooks from './ListBooks'
 import Search from './Search'
-import mapObject from './utils.js'
+import mapObject from './utils'
+import NoMatch from './NoMatch'
 
 class BooksApp extends React.Component {
-  
-	constructor(props){
+  constructor(props){
     super(props)
     this.props = props
     this.getChange = this.getChange.bind(this);
     this.doSearch = this.doSearch.bind(this);
     this.onAddBook = this.onAddBook.bind(this);
-   
     this.state = {
-      searchResults:false,  //list of results if present
+      searchResults:{},  //list of results if present
       selectedSearch:props.selectedSearch,  //book selected from search result
   	}
   }
@@ -29,44 +28,43 @@ class BooksApp extends React.Component {
       .then((books)=>{
         this.setState(()=>({
           books
-				}))
-			})
-  }
-
-  moveBooks = (book)=>{
-    this.setState((currentState)=>({
-      books: currentState.books.filter((c)=>{
-        return c.id !== book.id;
-      })
-    }))
-  }
-
-  getChange(a){
-    BooksAPI.update({'id':a.target.name},a.target.value)
-      .then(()=>{
-        BooksAPI.getAll()
-          .then((books)=>{
-            this.setState((currentState)=>({
-              books
-            })
-          )
-       })
+      }))
     })
+  }
+  
+  /*
+  * TODO: use isnew flag items or something
+  */
+  getChange(book,shelf,isnew){
+    BooksAPI.update(book,shelf)
+    .then((b)=>{
+      BooksAPI.getAll()
+        .then((books)=>{
+          this.setState(()=>({
+            books
+           }))//end setState
+        }) }//end Arrow func
+    )//end then
   }
 
   doSearch(e){
     const term = e.target.value
     if(term && term.length){
-      BooksAPI.search(e.target.value)
-				.then((res) => { 
+       BooksAPI.search(e.target.value)
+      	.then((res) => {  
         	const results = mapObject(res);
         	if(results.length) 
-            this.setState((state)=>({results}))
-				})
-    }else{
-			this.setState((state)=>({
-				results:false
-			}))
+              this.setState((state)=>({results}))
+          else{
+            //alert('no results')
+          }
+      })
+    }
+	else{ 
+      this.setState((state)=>({
+  		results:[]
+      })
+    )
     }
   }
 
@@ -76,59 +74,60 @@ class BooksApp extends React.Component {
     BooksAPI.update({id:bid},'wantToRead')
       .then((res)=>{
 	    BooksAPI.getAll()
-				.then((books)=>{
-					this.setState((state)=>({
-							books,
-						results:false
-					})
-				)})
-			})	
+		  .then((books)=>{
+			this.setState((state)=>({
+		      books,
+			  results:[]
+			})
+		  )
+		})
+    })	
   } 
 
   render(){
     return(
       <div className="app">
-       <Route exact path='/search' render={()=>(
+       <Route exact path='/search' render={() => (
           <Search 
-						searchResults={this.state.results}
+    		searchResults={this.state.results}
             selectedSearch={this.state.selectedSearch} 
-						doSearch={this.doSearch}
+			doSearch={this.doSearch}
             onAddBook={this.onAddBook}
             books={this.state.books}
             onChange={this.getChange}
            />
           )}
         />
-        <Route exact path='/' render={()=>(
-           <div className="list-books">
-             <div className="list-books-title">
-               <h1>MyReads</h1>
-             </div>
+        <Route exact path='/' render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
          	        <ListBooks 
-										books={this.state.books} 
-										shelf="currentlyReading" 
-										changeHandler={this.getChange}/>
+    			      books={this.state.books} 
+					  shelf="currentlyReading" 
+					  changeHandler={ this.getChange }/>
                   </div>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ListBooks 
-											books={this.state.books} 
+					  books={this.state.books} 
                       shelf="wantToRead" 
-                      changeHandler={this.getChange }/>
+                      changeHandler={ this.getChange }/>
                   </div>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ListBooks 
-											books={this.state.books} 
+					  books={this.state.books} 
                       shelf="read" 
                       changeHandler={this.getChange }/>
                   </div>
@@ -136,23 +135,28 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <Link className="open-search-btn" to='/search'>Add a book</Link>
+              <Link 
+				to='/search'
+				className="open-search-btn"
+				>Add a book</Link>
             </div>
           </div>
         )}/>
+
+       <Route path="*" component={NoMatch} status={404}/>
+ 
       </div>
     )
   }
 }
 
 BooksApp.propTypes = {
-  searchResults: PropTypes.object,
-  selectedSearch: PropTypes.object,
+  searchResults: PropTypes.array,
+  //selectedSearch: PropTypes.object,
   getChange: PropTypes.func,
   doSearch: PropTypes.func,
   getBook: PropTypes.func,
   onAddBook: PropTypes.func,
   changeHandler: PropTypes.func
 };
-
 export default BooksApp
