bd488f62e789a9c4feafab1802020e8d76d18d51
leroy-j <38360895+leroy-j@users.noreply.github.com>
2018-10-11 14:36:26 -0600

    Update NoMatch.js

--- home/src/NoMatch.js
+++ home/src/NoMatch.js
@@ -3,28 +3,28 @@ import { Link } from 'react-router-dom'
 
 //Handler for urls entered by user that does not match a component
 class NoMatch extends React.Component {
-  
+
   render(){
     const paths=['/search','/'];
-     return (
-       
-      (paths.indexOf(this.props.match.url, 0) === -1) && ( 
-     	<div className="page404">
-       		
-      		 
-       		 <center>
-     		  <h1 style={{position: 'relative'}}>404</h1>
-    		  <Link className="btn-link" to="/">Return to My Reads</Link></center>
-			  <hr />
-           
-		<nav>
-  		  <Link className="link-options" to="/">My Reads</Link> |
-		  <Link className="link-options" to="/search">Search</Link>
-        </nav>
-	  </div>
-	  )	
-    )
+    
+    return (
+     (paths.indexOf(this.props.match.url, 0) === -1) && 
+     (<div className="page404">
+       <center>
+        <h1 style={{position: 'relative'}}>404</h1>
+        <Link className="btn-link" to="/">Return to My Reads</Link>
+      </center>
+      <hr />
+
+      <nav>
+        <Link className="link-options" to="/">My Reads</Link> |	<Link className="link-options" to="/search">Search</Link>
+
+      </nav>
+     </div>))
    }
-  
+
+
 }
+
+
 export default NoMatch
